c20c44aefd390e43b33aef524ad2c45f0b39955b
sxwen <shixingwe.n@163.com>
2017-11-26 17:22:42 +0800

    WritingyourfirstDjangoapp,part5

--- python_project/develop_py/mysite/polls/views.py
+++ python_project/develop_py/mysite/polls/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http.response import HttpResponseRedirect,HttpResponse
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 """
 def index(request):
@@ -32,11 +33,17 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html' 
     context_object_name = 'latest_question_list'
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+                                       pub_date__lte = timezone.now() 
+                                       ).order_by('-pub_date')[:5]
+                                       
+    Question.objects.filter(pub_date__lte=timezone.now())
         
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'       
