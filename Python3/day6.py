4094c64bb3f5cb9853764ea28404e8aea3e3c907
shixingwe.n@163.com <shixingwe.n@163.com>
2020-02-24 20:20:13 +0800

    yes

--- Python3/day6.py
+++ Python3/day6.py
@@ -1,35 +1,62 @@
-###字典
-dict1 = {}
-dict2 = {'name':'guoguo','age':4,'sex':'girl'}
-##无序， 哈希算法， 键值对存储数据，python 中唯一的映射类型
+# ###字典
+# dict1 = {}
+# dict2 = {'name':'guoguo','age':4,'sex':'girl'}
+# ##无序， 哈希算法， 键值对存储数据，python 中唯一的映射类型
+#
+#
+# ###一切皆对象
+#
+# # a = 10
+# # print(id(a))
+#
+#
+# ###不可变类型： 整型，字典，元祖
+# ###可变类型：
+# ##字典创建，1,用工厂函数创建;2,
+# #工厂函数
+# #a = [1,2,3]
+# a= list((1,))
+# b = list([4])
+# print(a)
+# print(b)
+# ##字典的操作：
+# dict4 = dict((('name','guoguo'),))
+# print(dict4)
+# dict4['hobby']='boy'
+# dict4.setdefault('age',12)
+#
+# print(dict4)
+#
+# ##查找
+# print(dict4['name'])
+# print(dict4.keys())
+# print(list(dict4.keys()))
+# print(list(dict4.items()))
+# print(list(dict4.values()))
+# ##删除
+# #排序
+# print(sorted(dict4))
+# ##遍历
+# print('遍历')
+#
+# dict6 = {'name':'guoguo','age':4,'sex':'girl'}
+# for i in dict6:
+#     print(i,dict6[i])
+#
+# print('items')
+# for i,v in dict6.items():
+#
+#     print(i,v)
+#
+# print(dict6.items())
 
+####字符串############
+#String
+print('字符串')
+print('join 字符串拼接')
 
-###一切皆对象
+a = '123'
+b = 'adag'
 
-# a = 10
-# print(id(a))
-
-
-###不可变类型： 整型，字典，元祖
-###可变类型：
-##字典创建，1,用工厂函数创建;2,
-#工厂函数
-#a = [1,2,3]
-a= list((1,))
-b = list([4])
-print(a)
-print(b)
-##字典的操作：
-dict4 = dict((('name','guoguo'),))
-print(dict4)
-dict4['hobby']='boy'
-dict4.setdefault('age',12)
-
-print(dict4)
-
-##查找
-print(dict4['name'])
-print(dict4.keys())
-print(list(dict4.keys()))
-print(list(dict4.items()))
-print(list(dict4.values()))
+c = ''.join([a,b])
+print(c)
